6cfd44efecd83aec53de0a1c5900db3c8cd77d72
khanhcoluy <49581756+khanhcoluy@users.noreply.github.com>
2023-06-27 16:01:22 +0000

    sendFile using res.sendFile, and mount a middleware (express.static(absPath)) using app.use('assetFolderPath', middlewareFunc)

--- myApp.js
+++ myApp.js
@@ -2,8 +2,9 @@ let express = require('express');
 let app = express();
 
 const greetingFunc = (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(__dirname + '/views/index.html');
 }
+app.use('/public', express.static(__dirname + '/public'))
 app.get('/', greetingFunc);
 
 
