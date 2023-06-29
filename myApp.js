fcf73032296b7fc74334177f449ff6b56d8d62fc
khanhcoluy <49581756+khanhcoluy@users.noreply.github.com>
2023-06-29 14:28:29 +0000

    - learn how to use middleware to add a req.time which is currentTime and pass it value in response with JSON format (app.METHOD('path', 'middleware()', 'chainHandler()')

--- myApp.js
+++ myApp.js
@@ -1,14 +1,35 @@
 let express = require('express');
 let app = express();
 
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next();
+};
+
+app.use(loggerMiddleware);
+
 const greetingFunc = (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 }
 app.use('/public', express.static(__dirname + '/public'))
-app.get('/', greetingFunc);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/json', (req, res) => {
+  const messageStyle = process.env.MESSAGE_STYLE;
+  if (messageStyle === 'uppercase') {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
+})
 
-
-
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function handler(req, res) {
+  res.json({ time: req.time })
+})
 
 
 
